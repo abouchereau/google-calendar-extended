da6e3b5ff82193976a9b35b5b7619bd578689f8b
abouchereau <anthonybouchereau@hotmail.com>
2024-11-05 15:29:54 +0100

    https

--- src/server.js
+++ src/server.js
@@ -1,29 +1,52 @@
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require("express");
-const server = express();
 const GoogleCal = require("./GoogleCal");
 const cors = require("cors");
 const Sql = require("./Sql");
 
-var corsOptions = {
+
+
+const app = express();
+const corsOptions = {
     origin: ["https://cal.lasaugrenue.fr","http://localhost:8000"],
     optionsSuccessStatus: 200 // For legacy browser support
+};
+//const dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const key = fs.readFileSync(__dirname + '/selfsigned.key');
+const cert = fs.readFileSync(__dirname + '/selfsigned.crt');
+const options = {key, cert};
+const port = 3615;
+
+app.use(cors(corsOptions));
+app.use(express.json());
+let server = null;
+if (process.platform=="win32") {//local
+    server = http.createServer(options, app).listen(port, function(){
+        console.log("Express server listening on port " + port);
+      });
+} else {//serveur
+    server = https.createServer(options, app).listen(port, function(){
+        console.log("Express server listening on port " + port);
+      });
 }
 
-server.use(cors(corsOptions));
-server.use(express.json());
+
+
 
 const gCal = new GoogleCal();
 const sql = new Sql();
 
-server.get("/loadAllEvents",async (req, res)=> {
+app.get("/loadAllEvents",async (req, res)=> {
     await gCal.loadAllEvents();
 });
 
-server.get("/loadLastEvents",async (req, res)=> {
+app.get("/loadLastEvents",async (req, res)=> {
     await gCal.loadLastEvents();
 });
 
-server.get("/getEventList",async (req, res)=> {           
+app.get("/getEventList",async (req, res)=> {           
     const cal   = req.query.cal==null?null:parseInt(req.query.cal); //y a mieux à faire ?
     const year  = req.query.year==null?null:parseInt(req.query.year);
     const month = req.query.month==null?null:parseInt(req.query.month);
@@ -31,18 +54,18 @@ server.get("/getEventList",async (req, res)=> {
     res.send(list);
 });
 
-server.get("/getEvent/:id",async (req, res)=> {           
+app.get("/getEvent/:id",async (req, res)=> {           
     let event = await sql.getEvent(req.params.id);
     res.send(event);
 });
 
 
-server.get("/getCalList",async (req, res)=> {            
+app.get("/getCalList",async (req, res)=> {            
     let list = await sql.getCalList();
     res.send(list);
 });
 
-server.post("/updateEvent/:id",async (req, res)=> {    
+app.post("/updateEvent/:id",async (req, res)=> {    
     let item = req.body;
     let cal_id = item.cal_id;
     delete item.date_start;
@@ -54,4 +77,4 @@ server.post("/updateEvent/:id",async (req, res)=> {
     res.send(req.body);
 });
   
-server.listen(3615,()=>{console.log("Le serveur est lancé");})
+//server.listen(3615,()=>{console.log("Le serveur est lancé");})
