e1356829a2ed977a664b162f4b815f80b7589207
abouchereau <anthonybouchereau@hotmail.com>
2024-11-09 05:57:39 +0100

    jjjj

--- src/GoogleCal.js
+++ src/GoogleCal.js
@@ -1,4 +1,4 @@
-import fs from 'fs';//).promises;
+import { promises as fs } from 'fs';
 import {authenticate} from '@google-cloud/local-auth';
 import {google} from 'googleapis';
 import SqlEvent from './SqlEvent.js';
