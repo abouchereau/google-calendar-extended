12be47e43d34995184f85c45d9d8812d2712c54b
abouchereau <anthonybouchereau@hotmail.com>
2024-11-09 05:58:32 +0100

    kkk

--- src/SqlCal.js
+++ src/SqlCal.js
@@ -3,7 +3,7 @@ import SqlBase from "./SqlBase.js";
 
 export default class SqlCal extends SqlBase {
 
-    async createOrUpdateCal(cal) {
+    async insertOrUpdateCal(cal) {
         await this._query(
             "insert into cal(cal_id, color_front, color_back, summary) values(?,?,?,?) ON DUPLICATE KEY UPDATE color_front=VALUES(color_front), color_back=VALUES(COLOR_BACK), summary=VALUES(summary)",
             [cal.id, cal.foregroundColor, cal.backgroundColor, cal.summary]
