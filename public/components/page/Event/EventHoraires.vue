39c1708b4e68a7e623572bffefea74178267d428
abouchereau <anthonybouchereau@hotmail.com>
2024-12-02 11:54:59 +0100

    infos set

--- public/components/page/Event/EventHoraires.vue
+++ public/components/page/Event/EventHoraires.vue
@@ -8,36 +8,36 @@
             <div class="row">
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Lieu RDV</label>
-                    <input :disabled="!editable" id="lieuRdv" type="text" v-model="$main.item.lieuRdv"  class="form-control" />
+                    <input :disabled="!editable" id="lieuRdv" type="text" v-model="$main.item.lieuRdv" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure Début Concert</label>
-                    <input :disabled="!editable" id="heureDebutConcert" type="time" step="300" v-model="$main.item.heureDebutConcert"  class="form-control" />
+                    <input :disabled="!editable" id="heureDebutConcert" type="time" step="300" v-model="$main.item.heureDebutConcert" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure Balances</label>
-                    <input :disabled="!editable" id="heureBalance" type="time" step="300" v-model="$main.item.heureBalance"  class="form-control" />
+                    <input :disabled="!editable" id="heureBalance" type="time" step="300" v-model="$main.item.heureBalance" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure RDV Repas</label>
-                    <input :disabled="!editable" id="heureRdvRepas" type="time" step="300" v-model="$main.item.heureRdvRepas"  class="form-control" />
+                    <input :disabled="!editable" id="heureRdvRepas" type="time" step="300" v-model="$main.item.heureRdvRepas" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure Arrivée</label>
-                    <input :disabled="!editable" id="heureArrivee" type="time" step="300" v-model="$main.item.heureArrivee"  class="form-control" />
+                    <input :disabled="!editable" id="heureArrivee" type="time" step="300" v-model="$main.item.heureArrivee" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure Départ (calculée)</label>
-                    <input disabled id="heureDepart" type="time" step="300" v-model="$main.item.heureDepart"  class="form-control" />
+                    <input disabled id="heureDepart" type="time" step="300" v-model="$main.item.heureDepart" class="form-control" />
                 </div>
                 <div class="col-xl-3 col-lg-4 col-sm-12 py-1">
                     <label>Heure Retour</label>
-                    <input :disabled="!editable" id="heureRetour" type="time" step="300" v-model="$main.item.heureRetour"  class="form-control" />
+                    <input :disabled="!editable" id="heureRetour" type="time" step="300" v-model="$main.item.heureRetour" class="form-control" />
                 </div>
 
-                <div class="col-xl-6 col-lg-8 col-sm-12 py-1">
+                <div class="col-xl-12 col-lg-12 col-sm-12 py-1">
                     <label>Infos Set</label>
-                    <textarea :disabled="!editable" id="heureConcerts" v-model="$main.item.description"  class="form-control" />  
+                    <div class="alert alert-light border border-info" role="alert" v-html="$main.item.description"></div>
                 </div>
             </div>
         </div>
