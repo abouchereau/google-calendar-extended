b3ea7fbd847b35b0c12aa84726c916977b41713b
abouchereau <anthonybouchereau@hotmail.com>
2025-03-21 11:24:28 +0100

    mjk

--- public/components/page/Panel/PanelTransports.vue
+++ public/components/page/Panel/PanelTransports.vue
@@ -2,10 +2,10 @@
 <template>
     <div v-if="item.transports!=null && item.transports.includes('1')">
         <div class="row">
-            <div class="col-3 align-self-center">
+            <div class="col-md-3 col-sm-12 align-self-center text-center">
             <i class="fa-solid fa-van-shuttle fa-big text-primary" title="Crafter"></i>
             </div>                  
-            <div class="col-9">
+            <div class="col-md-9 col-sm-12">
             <div><i class="fa fa-square-caret-right text-warning"></i> <span class="fs-sm" v-if="item.dateDepartCrafter">{{ dayCrafter(item.dateDepartCrafter)}}</span><span class="text-danger" v-else>N.C.</span></div>
             <div><i class="fa fa-square-caret-left text-success"></i> <span class="fs-sm" v-if="item.dateRetourCrafter">{{ dayCrafter(item.dateRetourCrafter)}}</span><span class="text-danger" v-else>N.C.</span></div>
             </div>
@@ -13,30 +13,30 @@
     </div>
     <div v-if="item.transports!=null && item.transports.includes('2')">
         <div class="row">
-            <div class="col-3 align-self-center">
+            <div class="col-md-3 col-sm-12 align-self-center text-center">
             <i class="fa-solid fa-car-side fa-big text-info" title="Véhicule(s) perso"></i>
             </div>                  
-            <div class="col-9">
+            <div class="col-md-9 col-sm-12">
               <span class="badge bg-info" v-if="item.vehiculesPerso">{{ item.vehiculesPerso.substring(0,25) }}</span>
             </div>     
         </div>    
     </div>
     <div v-if="item.transports!=null && item.transports.includes('3')">
         <div class="row">
-            <div class="col-3 align-self-center">
+            <div class="col-md-3 col-sm-12 align-self-center text-center">
             <i class="fa-solid fa-ambulance fa-big text-warning" title="Location"></i>
             </div>                  
-            <div class="col-9">
+            <div class="col-md-9 col-sm-12">
               <span class="badge bg-warning" v-if="item.location">{{ item.location.substring(0,25) }}</span>
             </div>     
         </div>    
     </div>
     <div v-if="item.transports!=null && item.transports.includes('4')">
         <div class="row">
-            <div class="col-3 align-self-center">
+            <div class="col-md-3 col-sm-12 align-self-center text-center">
             <i class="fa-solid fa-train fa-big text-success" title="Train"></i>
             </div>                  
-            <div class="col-9">
+            <div class="col-md-9 col-sm-12">
               <span class="badge bg-success" v-if="item.train">{{ item.train.substring(0,25) }}</span>
             </div>     
         </div>    
