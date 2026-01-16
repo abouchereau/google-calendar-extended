aa97a717d5293bf723297d4db266ff3af5c12f4d
abouchereau <anthonybouchereau@hotmail.com>
2026-01-16 14:04:51 +0100

    add dev origin

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import jwt from 'jsonwebtoken';
 const JWT_SECRET = 'lasaugrenue';
 const app = express();
 const corsOptions = {
-    origin: ["https://cal.lasaugrenue.fr","http://localhost:8000", "https://www.lasaugrenue.fr"],
+    origin: ["https://cal.lasaugrenue.fr","https://cal-dev.lasaugrenue.fr","http://localhost:8000", "https://www.lasaugrenue.fr"],
     optionsSuccessStatus: 200 
 };
 
